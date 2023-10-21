1a62429f42165440e0238f8d76bbc46d7caf3056
Alex Soto <alex@soto.dev>
2023-10-21 10:28:40 -0400

    Fix our Initialize story

---
+++ PSPDFKit.dotnet.Android/Additions/PSPDFKitGlobal.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Android.Content;
+using Java.Interop;
+
+[assembly: MetaData (
+	name: "pspdfkit_automatic_initialize",
+	Value = "false"
+)]
+
+namespace PSPDFKit {
+
+	public sealed partial class PSPDFKitGlobal {
+
+		internal const string ProductIdentifier = "DotNetBindingsAndroid";
+
+		public static void Initialize (Context context, string? licenseKey, IList<string> fontPaths)
+		{
+			if (context is null)
+				throw new NullReferenceException ($"context");
+
+			if (fontPaths is null)
+				throw new NullReferenceException ($"fontPaths");
+
+			Initialize (context, licenseKey, fontPaths, ProductIdentifier);
+		}
+
+		public static void Initialize (Context context, string? licenseKey)
+		{
+			if (context is null)
+				throw new NullReferenceException($"context");
+
+			Initialize (context, licenseKey, Array.Empty<string>(), ProductIdentifier);
+		}
+	}
+}
