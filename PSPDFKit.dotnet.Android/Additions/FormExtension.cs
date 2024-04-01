d3b650c122803ddeed55609959e3d4766dc5d290
Alex Soto <alex@soto.dev>
2024-04-01 19:52:32 -0400

    [PSPDFKit.Android] Update bindings to v2024.1.2

---
+++ PSPDFKit.dotnet.Android/Additions/FormExtension.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Android.Runtime;
+
+namespace PSPDFKit.Forms
+{
+    public partial class TextFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class SignatureFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class RadioButtonFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class PushButtonFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class ListBoxFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class ComboBoxFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public partial class CheckBoxFormConfiguration
+    {
+        public partial class Builder
+        {
+            protected override unsafe global::Java.Lang.Object RawThis
+            {
+
+                [Register("getThis", "()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;", "GetGetThisHandler")]
+                get
+                {
+                    const string __id = "getThis.()Lcom/pspdfkit/forms/FormElementConfiguration$BaseBuilder;";
+                    try
+                    {
+                        var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                        return (global::Java.Lang.Object?)global::Java.Lang.Object.GetObject<global::Java.Lang.Object>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+}
+
